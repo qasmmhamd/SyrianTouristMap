a9582d3c4b015080536c3b9b0ef65a09b4f26868
abdUlkaderEng <abdulkaderbakdunes@gmail.com>
2025-11-16 21:25:57 +0300

    fix: update allowed_origins in CORS configuration for local development

--- config/cors.php
+++ config/cors.php
@@ -4,8 +4,9 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods'   => ['*'],
     'allowed_origins'   => [
-        'https://annaponsprojects.com',
+        'http://127.0.0.1:3000',
         'http://localhost:3000',
+
     ],
     'allowed_origins_patterns' => [
         '/^https?:\/\/([a-z0-9-]+\.)?annaponsprojects\.com$/',
